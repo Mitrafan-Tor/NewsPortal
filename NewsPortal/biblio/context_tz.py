876b68be0d2815d917668c5d4b6a271407660046
Mitrafan-Tor <dmitrytorzhikov@gmail.com>
2025-05-10 17:54:04 +0400

    Смена темы в зависимости от времени

--- NewsPortal/biblio/context_tz.py
+++ NewsPortal/biblio/context_tz.py
@@ -2,9 +2,19 @@ import pytz
 from django.utils import timezone
 from django.conf import settings
 
+# def get_timezone(request):
+#     return {
+#         'current_time': timezone.now(),
+#         'timezones': pytz.common_timezones,
+#         'TIME_ZONE': request.session.get('django_timezone', settings.TIME_ZONE)
+#     }
+
 def get_timezone(request):
+    tzname = request.session.get('django_timezone', settings.TIME_ZONE)
+    local_time = timezone.localtime(timezone.now(), pytz.timezone(tzname))
+
     return {
-        'current_time': timezone.now(),
+        'current_time': local_time,
         'timezones': pytz.common_timezones,
-        'TIME_ZONE': request.session.get('django_timezone', settings.TIME_ZONE)
+        'TIME_ZONE': tzname
     }
